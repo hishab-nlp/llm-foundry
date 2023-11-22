b933bb25e79dcefbaa5655082fd713692ec11f7b
sagorsarker <sagor.sarker@hishab.co>
2023-11-22 06:14:05 +0000

    update pretrain run bash file

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,7 @@
 composer train/train.py \
   train/yamls/pretrain/mpt-1b-bn.yaml \
-  data_local=/llm-foundry/dataset \
   train_loader.dataset.split=train \
   eval_loader.dataset.split=val \
-  max_duration=2ba \
-  eval_interval=0
+  max_duration=2ep \
+  eval_interval=10000ba
   # save_folder=mpt-125m-bn-1
